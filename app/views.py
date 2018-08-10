3d58ef4c667d524ca0109ddbd0bc2aafaf9de16a
ramza007 <ramzalvo007@gmail.com>
2018-08-10 13:08:07 +0300

    adding the movie api

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+# Views functions
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Hello to the movie review website'
+    return render_template('index.html', title = title)
+
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    title = 'id'
+    return render_template('movie.html', id=movie_id)
