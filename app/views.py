e47e8fd16887a28dc0b1142d4e32ca8c0d0174c0
ramza007 <ramzalvo007@gmail.com>
2018-08-11 00:03:11 +0300

    added styling, posters and other styling

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies, get_movie
 
 # Views functions
 
@@ -8,8 +9,12 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Hello to the movie review website'
-    return render_template('index.html', title = title)
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    title = 'The movie review website'
+    return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie, now_showing=now_showing_movie)
 
 
 @app.route('/movie/<int:movie_id>')
@@ -17,5 +22,7 @@ def movie(movie_id):
     '''
     View movie page function that returns the movie details page and its data
     '''
-    title = 'id'
-    return render_template('movie.html', id=movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
