394be95ff3a588f09147bcbca023aa888fbecf6b
ramza007 <ramzalvo007@gmail.com>
2018-08-12 18:19:57 +0300

    configured search function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_movies, get_movie, search_movie
 
 # Views functions
+
 
 @app.route('/')
 def index():
@@ -14,7 +15,12 @@ def index():
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
     title = 'The movie review website'
-    return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie, now_showing=now_showing_movie)
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
 @app.route('/movie/<int:movie_id>')
@@ -37,5 +43,5 @@ def search(movie_name):
     movie_name_list = movie_name.split(" ")
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
-    title = f'search results for {movie_name}'
-    return render_template('search.html', movies=searched_movies)
+    title = f'Search results for "{movie_name}"'
+    return render_template('search.html', movies=searched_movies, title=title)
