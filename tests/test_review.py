3b835f1de49f73b0cdc559592b7e4b5a7000f797
ramza007 <ramzalvo007@gmail.com>
2018-08-13 15:06:49 +0300

    added tests and test cases

--- tests/test_review.py
+++ tests/test_review.py
@@ -1,30 +1,33 @@
 import unittest
-from app.models import Movie
+from app.models import Review
 
-class Review(unitest.Testcase):
 
-    all_reviews = []
+class TestReview(unittest.TestCase):
 
-    def __init__(self, movie_id, title, imageurl, review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
+    def setUp(self):
+        self.new_review = Review(12345, 'Review for movies', "https://image.tmdb.org/t/p/w500/jdjdjdjn",
+                                 'This movie is the best thing since sliced bread')
 
-    def save_review(self):
-        Review.all_reviews.append(self)
+    def tearDown(self):
+        Review.clear_reviews()
 
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_review, Review))
 
-    @classmethod
-    def get_reviews(cls, id):
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_review.movie_id, 12345)
+        self.assertEquals(self.new_review.title, 'Review for movies')
+        self.assertEquals(self.new_review.imageurl,
+                          "https://image.tmdb.org/t/p/w500/jdjdjdjn")
+        self.assertEquals(self.new_review.review,
+                          'This movie is the best thing since sliced bread')
 
-        response = []
+    def test_save_review(self):
+        self.new_review.save_review()
+        self.assertTrue(len(Review.all_reviews) > 0)
 
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
+    def test_get_review_by_id(self):
 
-        return response
+        self.new_review.save_review()
+        got_reviews = Review.get_reviews(12345)
+        self.assertTrue(len(got_reviews) == 1)
